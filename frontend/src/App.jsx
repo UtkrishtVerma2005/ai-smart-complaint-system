eefd1bf0d6936d34b5f1412022f55431ccc7e396
Utkrisht Verma <utkrishtverma99@gmail.com>
2026-05-19 11:59:04 +0530

    updated frontend ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,41 @@
 import {
- BrowserRouter,
- Routes,
- Route
+   BrowserRouter,
+   Routes,
+   Route
 }
 from "react-router-dom";
 
-import Login
-from "./pages/Login";
-
-import Signup
-from "./pages/Signup";
-
-import Home
-from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 function App(){
 
-  return(
+   return(
 
-    <BrowserRouter>
+      <BrowserRouter>
 
-      <Routes>
+         <Routes>
 
-        <Route
-        path="/"
-        element={<Login />}
-        />
+            <Route
+            path="/"
+            element={<Login />}
+            />
 
-        <Route
-        path="/signup"
-        element={<Signup />}
-        />
+            <Route
+            path="/signup"
+            element={<Signup />}
+            />
 
-        <Route
-        path="/home"
-        element={<Home />}
-        />
+            <Route
+            path="/home"
+            element={<Home />}
+            />
 
-      </Routes>
+         </Routes>
 
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+   );
 }
 
 export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,38 +4,49 @@ from "../components/ComplaintForm";
 import Complaints
 from "./Complaints";
 
+import { useNavigate }
+from "react-router-dom";
+
 function Home(){
+
+   const navigate =
+   useNavigate();
+
+   const handleLogout = ()=>{
+
+      localStorage.removeItem(
+         "token"
+      );
+
+      navigate("/");
+   };
 
    return(
 
-      <div className="main-layout">
+      <div className="container">
 
-         <div className="left-panel">
+         <div className="circle1"></div>
 
-            <div className="container">
+         <div className="circle2"></div>
 
-               <div className="circle1"></div>
+         <button
+         className="logout-btn"
+         onClick={handleLogout}
+         >
 
-               <div className="circle2"></div>
+            Logout
 
-               <h1 className="title">
+         </button>
 
-                  AI Smart
-                  Complaint System
+         <h1 className="title">
 
-               </h1>
+            AI Smart Complaint System
 
-               <ComplaintForm />
+         </h1>
 
-            </div>
+         <ComplaintForm />
 
-         </div>
-
-         <div className="right-panel">
-
-            <Complaints />
-
-         </div>
+         <Complaints />
 
       </div>
    );
